c22c54718fc078e12b9f9fd60700ceddc1e12e17
EdgeCourse <106573186+EdgeCourse@users.noreply.github.com>
2025-08-20 15:06:28 -0500

    Update App.jsx

--- React Debugging Techniques/src/App.jsx
+++ React Debugging Techniques/src/App.jsx
@@ -1,15 +1,17 @@
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-//import './BuggyComponent1'
-import './BuggyParent2'
+import { BuggyComponent1 } from './BuggyComponent1'
+
 function App() {
-{/*<BuggyComponent1 />*/}
-{/*<BuggyParent2 />*/}
-{/*<BuggyButton3 />*/}
-{/*<BuggyList4 />*/}
-{/*<BuggyInfiniteRender5 />*/}
+  return (
+    <div>
+      <h1>My Vite + React App</h1>
+      <BuggyComponent1 />
+    </div>
+  );
 }
 
-export default App
+export default App;
